ec0af6e1f790f463a0b4cb1418ac16d2267efade
neutron219 <isaiaseortega21@gmail.com>
2023-01-24 08:47:49 -0300

    Subiendo el Proyecto

---
+++ README.md
@@ -0,0 +1,2 @@
+ ¡Entraga del CV para el proyecto final del curso proporcionado por la academia ticmas!
+
---
+++ js/funciones.js
@@ -0,0 +1,4 @@
+
+document.getElementById('boton').onclick = function () {
+    document.getElementById('divjs').innerHTML = "Gracias por Contactarme, tus datos proporcionados en el formulario<br> seran registrado y en el tiempo más breve posible me contactaré <br>contigo por los medios proporcionamos."
+}
